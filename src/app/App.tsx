a47616b9088d8e49d8215ebc2869a46a234f5dc1
Bekzat Abdibakas <babdibakas@yandex.kz>
2025-03-04 22:38:48 +0500

    feat: added ui kit AppInput

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,22 @@
-import * as styles from './App.module.scss'
 import {AppButton} from "shared/ui/AppButton/AppButton";
+import {useState} from "react";
+import {AppInput} from "shared/ui/AppInput/AppInput";
 
 const App = () => {
+
+    const [value, setValue] = useState<string>('')
+
+    const onChangeValue = (value: string) => {
+        setValue(value)
+    }
+
     return (
         <div className="wrapper">
             <div className="container">
-                <p className={styles.text}>
-                    Hello, World!
-                    <AppButton onClick={() => alert("Hello")}>
-                        Login
-                    </AppButton>
-                </p>
+                <AppInput value={value} placeholder={'Value'} onChange={onChangeValue}/>
+                <AppButton onClick={() => alert("Hello")}>
+                    Login
+                </AppButton>
             </div>
         </div>
     )
